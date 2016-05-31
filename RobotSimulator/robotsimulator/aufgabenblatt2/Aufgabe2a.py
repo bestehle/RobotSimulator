0cf99d6159a3209231a2e3655dc5a908456a4854
deparlak <deparlak@googlemail.com>
2016-05-31 17:24:00 +0200

    - added missing file

---
+++ RobotSimulator/robotsimulator/aufgabenblatt2/Aufgabe2a.py
@@ -0,0 +1,15 @@
+from robotsimulator import Robot
+from robotsimulator.graphics.graphics import Point
+from robotsimulator.worlds import obstacleWorld2 as obstacleWorld
+
+
+myWorld = obstacleWorld.buildWorld()
+polyline = [Point(1, 5), Point(18, 5)]
+myWorld.drawPolyline(polyline)
+
+myRobot = Robot.Robot()
+myWorld.setRobot(myRobot, 1, 5, 0)
+
+myRobot.followPolylineWithObstacle(1, polyline, 6, 1)
+
+myWorld.close()
