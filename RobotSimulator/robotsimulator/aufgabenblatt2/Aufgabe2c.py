62545406b4f84e032aa0b36407efd33a6b5b6b6e
deparlak <deparlak@googlemail.com>
2016-06-01 11:18:15 +0200

    added missing demo

---
+++ RobotSimulator/robotsimulator/aufgabenblatt2/Aufgabe2c.py
@@ -0,0 +1,15 @@
+from robotsimulator import Robot
+from robotsimulator.graphics.graphics import Point
+from robotsimulator.worlds import obstacleWorld3 as obstacleWorld
+
+
+myWorld = obstacleWorld.buildWorld()
+polyline = [Point(1, 6), Point(10, 6), Point(10.5, 3)]
+myWorld.drawPolyline(polyline)
+
+myRobot = Robot.Robot()
+myWorld.setRobot(myRobot, 1, 6, 0)
+
+myRobot.followPolylineWithObstacle(1, polyline, 6, 1)
+
+myWorld.close()
