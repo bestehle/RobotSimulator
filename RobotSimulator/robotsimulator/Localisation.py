7374e4246c1a046644b39a0d6ec10a2131210bbd
Benjamin Stehle <bestehle@htwg-konstanz.de>
2016-06-14 10:09:21 +0200

    4) localize Obstacles in world

--- RobotSimulator/robotsimulator/Localisation.py
+++ RobotSimulator/robotsimulator/Localisation.py
@@ -24,8 +24,22 @@ class Localistaion(object):
     def generateParticles(self, particles=100):
         xValues = numpy.random.laplace(0, 3, particles)
         yValues = numpy.random.laplace(0, 3, particles)
-        thetaValues = map(lambda x: x * math.pi * 2, numpy.random.random(20))
+        thetaValues = map(lambda x: x * math.pi * 2, numpy.random.random(particles))
         weightValues = [0] * particles
         
         return zip(xValues, yValues, thetaValues, weightValues)
         
+    def localizeObstacles(self, position):
+        positions = map(self.calculatePosition, self._robot.getSensorDirections(),
+                        self._robot.sense(), [position] * len(self._robot.sense()))
+        
+        
+        
+    def calculatePosition(self, direction, distance, position):
+        if distance == None:
+            return None
+        absDirection = position[2] + direction
+        x = math.cos(absDirection) * distance
+        y = math.sin(absDirection) * distance
+        
+        return position[0] + x, position[1] + y
